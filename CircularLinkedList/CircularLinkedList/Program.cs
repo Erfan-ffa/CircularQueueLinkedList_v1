f84afc399c5c44d2dfd6ada77de279857a850d81
Erfan Faridi <109587086+Erfan-ffa@users.noreply.github.com>
2022-08-18 12:08:04 +0430

    Update Program.cs

--- CircularLinkedList/CircularLinkedList/Program.cs
+++ CircularLinkedList/CircularLinkedList/Program.cs
@@ -1,17 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
 using CircularLinkedList;
 
 Operation x = new Operation();
 x.Dequeue();
-x.Enqueue(0);
-x.Dequeue();
-x.Dequeue();
 x.Enqueue(1);
-x.Enqueue(2);
 x.Dequeue();
+x.Enqueue(2);
 x.Enqueue(3);
 x.Enqueue(4);
-x.Dequeue();
 x.Enqueue(5);
+
+x.Dequeue();
+x.Dequeue();
+x.Dequeue();
+x.Dequeue();
+x.Dequeue();
 
 x.Print();
