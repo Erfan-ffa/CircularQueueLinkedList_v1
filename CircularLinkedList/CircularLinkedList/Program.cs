f5da377315bede9c69e71bc0aa402efe76bd47dc
Erfan Faridi <109587086+Erfan-ffa@users.noreply.github.com>
2022-08-17 21:35:10 -0700

    Add files via upload

---
+++ CircularLinkedList/CircularLinkedList/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CircularLinkedList;
+
+Operation x = new Operation();
+x.Dequeue();
+x.Enqueue(0);
+x.Dequeue();
+x.Dequeue();
+x.Enqueue(1);
+x.Enqueue(2);
+x.Dequeue();
+x.Enqueue(3);
+x.Enqueue(4);
+x.Dequeue();
+x.Enqueue(5);
+
+x.Print();
